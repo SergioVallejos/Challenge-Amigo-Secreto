9f5d46f7f03b055afcd7598efe2a8b64fa8d2f54
Sergio Vallejos <sergioemanuelvallejos@gmail.com>
2025-08-11 17:02:15 -0300

    Función agregar amigo

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+function agregarAmigo(){
+
+    let nombreDeAmigo = document.querySelector("#amigo").value;
+
+    if(nombreDeAmigo === "") {
+
+        alert("El campo esta vacio, ingrese un nombre por favor");
+    }
+     else {
+        amigos.push(nombreDeAmigo);
+        document.querySelector("#amigo").value;
+
+}
+}
